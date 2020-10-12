cfc7edfb55caf8981c6b9b1c2ee9fa142fc8bfd4
Tiago Freire <tmiguelf@hotmail.com>
2020-10-12 20:05:53 +0200

    Added quick Visual studio templates to use quickMSBuild project

---
+++ quickTemplates/generator/resources/resources/versionSpecific.h
@@ -0,0 +1,19 @@
+//======== ======== ======== ======== ======== ======== ======== ========
+///	\file
+///		Version information
+///
+///	\copyright
+///		
+//======== ======== ======== ======== ======== ======== ======== ========
+
+#pragma once
+
+#define VERSION_MAJOR	0
+#define VERSION_MINOR	0
+#define VERSION_REV		0
+
+#define VERSION_PROGRAMNAME			"$projectname$"
+#define VERSION_PROGRAMDESCRIPTION	"$projectname$"
+
+#define VERSION_COMPANYNAME	""
+#define VERSION_COPYRIGHT	""
