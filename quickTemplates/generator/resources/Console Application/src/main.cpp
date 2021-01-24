62d2563c7e6c50ea78e6b4d826329b5d251190f4
Tiago Freire <tmiguelf@hotmail.com>
2021-01-24 15:24:07 +0100

    Added EOL option

--- quickTemplates/generator/resources/Console Application/src/main.cpp
+++ quickTemplates/generator/resources/Console Application/src/main.cpp
@@ -6,9 +6,17 @@
 ///		
 //======== ======== ======== ======== ======== ======== ======== ========
 
+#ifdef _WIN32
+int wmain(
+	[[maybe_unused]] int argc,
+	[[maybe_unused]] wchar_t** argv,
+	[[maybe_unused]] wchar_t** envp)
+#else
 int main(
 	[[maybe_unused]] int argc,
-	[[maybe_unused]] char* argv[])
+	[[maybe_unused]] char** argv,
+	[[maybe_unused]] char** envp)
+#endif
 {
 	
 	return 0;
